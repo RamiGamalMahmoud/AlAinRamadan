b45e98c9363f369e26f330b0ec32315d4cd0a527
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-02-13 23:50:52 +0200

    Create new release 0.0.6

--- installer/pac.ps1
+++ installer/pac.ps1
@@ -1,10 +1,10 @@
 dotnet publish -c Release  ..\src\AlAinRamadan\AlAinRamadan -r win-x64 -o .\publish
 
-$version = "0.0.5"
+$version = "0.0.6"
 $pack_id = "AlAinRamadan"
 $main_exe = "AlAinRamadan.exe"
 $icon_path = ".\mosque.ico"
 $splash_image = ".\mosque.png"
-$framework = "net8.0-x64-desktop"
+$framework = "net9.0-x64-desktop"
 $pack_title = "Ain Ain Ramadan"
 vpk pack -u $pack_id -v $version -p .\publish -e $main_exe --icon $icon_path --splashImage $splash_image --framework $framework --packTitle $pack_title
