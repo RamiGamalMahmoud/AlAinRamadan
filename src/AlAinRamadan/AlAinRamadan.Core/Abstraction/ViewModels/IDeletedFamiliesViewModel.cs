4c434b20af439d62af1d59736c25f86318d16e71
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-02-23 22:20:41 +0200

    Create IDeletedFamiliesViewModel.cs

---
+++ src/AlAinRamadan/AlAinRamadan.Core/Abstraction/ViewModels/IDeletedFamiliesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using AlAinRamadan.Core.Models;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace AlAinRamadan.Core.Abstraction.ViewModels
+{
+    public interface IDeletedFamiliesViewModel
+    {
+        ObservableCollection<Family> Families { get; }
+        Task LoadAsync();
+    }
+}
