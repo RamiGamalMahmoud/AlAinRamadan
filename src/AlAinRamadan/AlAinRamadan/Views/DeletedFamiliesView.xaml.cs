078b3d5c08c52e5610e701728a806b335a5ac9f6
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-02-23 22:20:33 +0200

    create DeletedFamiliesView

---
+++ src/AlAinRamadan/AlAinRamadan/Views/DeletedFamiliesView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using AlAinRamadan.Core.Abstraction.ViewModels;
+using AlAinRamadan.Core.Abstraction.Views;
+using System.Windows;
+
+namespace AlAinRamadan.Views
+{
+    public partial class DeletedFamiliesView : Window, IDeletedFamiliesView
+    {
+        public DeletedFamiliesView(IDeletedFamiliesViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+            Loaded += (s, e) => Dispatcher.Invoke(async () => await viewModel.LoadAsync());
+        }
+    }
+}
