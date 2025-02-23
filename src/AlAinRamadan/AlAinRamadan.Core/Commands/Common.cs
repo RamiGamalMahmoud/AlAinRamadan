e29c89401afc0efe960dc0efcee4a5a54a2acf81
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-02-23 22:18:27 +0200

    add ShowDeletedCommand

--- src/AlAinRamadan/AlAinRamadan.Core/Commands/Common.cs
+++ src/AlAinRamadan/AlAinRamadan.Core/Commands/Common.cs
@@ -5,6 +5,7 @@ namespace AlAinRamadan.Core.Commands
 {
     public static class Common
     {
+        public record ShowDeletedCommand<T> : IRequest where T : class;
         public record ShowCreateCommand<TModel> : IRequest where TModel : class;
         public record ShowEditCommand<TModel>(TModel Model) : IRequest where TModel : class;
         public record DirectPrintCommand(string ReportName, string printerName, bool IsLabel, Dictionary<string, string> Parameters = null, Dictionary<string, object> DataSources = null) : IRequest;
