b34878e84e1c748702e2e40d86c9ecfdd98a0d44
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-02-23 22:20:11 +0200

    Create ShowDeletedCommandHandler.cs

---
+++ src/AlAinRamadan/AlAinRamadan/CommandHandlers/ShowDeletedCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using AlAinRamadan.Core.Abstraction.Views;
+using AlAinRamadan.Core.Commands;
+using AlAinRamadan.Core.Models;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AlAinRamadan.CommandHandlers
+{
+    internal class ShowDeletedCommandHandler(IServiceProvider services) : IRequestHandler<Core.Commands.Common.ShowDeletedCommand<Family>>
+    {
+        public Task Handle(Common.ShowDeletedCommand<Family> request, CancellationToken cancellationToken)
+        {
+            services.GetRequiredService<IDeletedFamiliesView>().Show();
+            return Task.CompletedTask;
+        }
+    }
+}
