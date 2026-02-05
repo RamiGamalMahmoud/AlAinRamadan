a5873f07e2684d6014e864377694fc9569d6b903
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2026-02-05 16:42:13 +0200

    Create new release 0.4.6

--- installer/pac.ps1
+++ installer/pac.ps1
@@ -1,6 +1,6 @@
 dotnet publish -c Release  ..\src\AlAinRamadan\AlAinRamadan -r win-x64 -o .\publish
 
-$version = "0.4.5"
+$version = "0.4.6"
 $pack_id = "AlAinRamadan"
 $main_exe = "AlAinRamadan.exe"
 $icon_path = ".\mosque.ico"
